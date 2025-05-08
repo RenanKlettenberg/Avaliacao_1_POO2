2bb21ce087bf5e2f6ff69fe6079dfc5399dbaea0
Renan G. Klettenberg <121028916+RenanKlettenberg@users.noreply.github.com>
2025-05-08 20:52:04 -0300

    Model Cliente

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,7 +1,18 @@
 package models;
 
-public class Cliente {
-    
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nome;
     private String cpf;
     private String telefone;
@@ -12,6 +23,14 @@ public class Cliente {
         this.cpf = cpf;
         this.telefone = telefone;
         this.pontos = pontos;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -44,6 +63,31 @@ public class Cliente {
 
     public void setPontos(int pontos) {
         this.pontos = pontos;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Cliente[ id=" + id + " ]";
     }
     
 }
